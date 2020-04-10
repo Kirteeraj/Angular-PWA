09dacb91b85c884bfd433bdeaa753791b169d43b
unknown <kirteerajmalkar@gmail.com>
2020-04-10 22:55:18 +0530

    Initial Commit - Home section,Nevegation,Contact Section,etcs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCoffee ,faList} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PortfolioPriject';
+  faCoffee=faCoffee;
+  faList=faList;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub, faCodepen, faInstagram, faFacebook, faLinkedin,faGoogle} from '@fortawesome/free-brands-svg-icons';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component' ;
+
+library.add(faGithub,faCodepen,faInstagram,faFacebook,faLinkedin,faGoogle);
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+
+
+SidebarOn(){
+  var vid = document.getElementsByTagName("video-background-2")[0];   
+  var att = document.createAttribute("autoplay");  
+  vid.setAttributeNode(att); 
+  document.write("Hello World!");
+}
+SidebarOff(){
+  
+  }
+}
